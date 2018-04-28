e9f1e8864eb60b9a3f042db74cac12c3741b7ea5
kylegilde <kylegilde@gmail.com>
2018-04-28 16:36:27 -0500

    4/28

--- Final Project/exploration.R
+++ Final Project/exploration.R
@@ -1,4 +1,4 @@
-load_packages <- c("prettydoc", "jsonlite", "knitr")
+load_packages <- c("prettydoc", "jsonlite", "knitr", "tidyverse")
 
 #see if we need to install any of them
 install_load <- function(pkg){
@@ -16,9 +16,12 @@ View(my_results)
 rm(crime_df)
 crime_df <- read.csv("https://data.cityofchicago.org/api/views/ijzp-q8t2/rows.csv?accessType=DOWNLOAD")
 
-crime_df_v2 <- crime_df %>% select(c(Date, Primary.Type, Description, Location, Arrest, 
+crime_df_v2 <- crime_df %>% dplyr::select(c(Date, Primary.Type, Description, Location, Arrest, 
                                       Domestic, Community.Area, Year, Latitude, Longitude))
 
 write.csv(crime_df_v2, "crime_df_v2.csv")
+crime_df_v2 <- read.csv("crime_df_v2.csv")
 
-View(crime_df_v2)
+crime_df_ <- 
+
+View(head(crime_df_v2,100))
