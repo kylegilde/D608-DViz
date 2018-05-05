fda7b3c420094313dcb28816e3789f29efac1b8e
kylegilde <kylegilde@gmail.com>
2018-05-05 18:59:26 -0500

    4/28

--- Final Project/exploration.R
+++ Final Project/exploration.R
@@ -1,4 +1,4 @@
-load_packages <- c("prettydoc", "jsonlite", "knitr", "tidyverse")
+
 
 #see if we need to install any of them
 install_load <- function(pkg){
@@ -8,20 +8,54 @@ install_load <- function(pkg){
   sapply(pkg, require, character.only = TRUE, quietly = TRUE)
 }
 
+load_packages <- c("prettydoc", "jsonlite", "knitr", "lubridate",
+                   "tidyverse", "anytime", "data.table")
 install_load(load_packages)
 
 
-View(my_results)
+# View(my_results)
+# 
+# rm(crime_df)
+# crime_df <- read.csv("https://data.cityofchicago.org/api/views/ijzp-q8t2/rows.csv?accessType=DOWNLOAD")
+# 
+# crime_df_v2 <- crime_df %>% dplyr::select(c(Date, Primary.Type, Description, Location, Arrest, 
+#                                       Domestic, Community.Area, Year, Latitude, Longitude))
+# 
+# write.csv(crime_df_v2, "crime_df_v2.csv")
 
-rm(crime_df)
-crime_df <- read.csv("https://data.cityofchicago.org/api/views/ijzp-q8t2/rows.csv?accessType=DOWNLOAD")
 
-crime_df_v2 <- crime_df %>% dplyr::select(c(Date, Primary.Type, Description, Location, Arrest, 
-                                      Domestic, Community.Area, Year, Latitude, Longitude))
+setwd("C:\\Users\\kyleg")
+crime_df_v2 <- data.table(read.csv("crime_df_v2.csv"))
+str(crime_df_v2)
 
-write.csv(crime_df_v2, "crime_df_v2.csv")
-crime_df_v2 <- read.csv("crime_df_v2.csv")
+crime_samp <- sample_n(crime_df_v2, 1000)
+# crime_tab <- table(crime_samp$Primary.Type)
+# 
+# crime_samp2 <- data.frame(
+#   crime = names(crime_tab),
+#   incidents = as.integer(crime_tab)
+# )
+# write.csv(crime_samp2, "crime_samp2.csv")
+# 
+# write.csv(crime_samp, "crime_samp.csv")
 
-crime_df_ <- 
+crime_samp2 <- 
+  crime_samp %>% 
+  mutate(
+    originalDate = Date,
+    DateTime = lubridate::mdy_hms(as.character(Date)),
+    Date = as.Date.POSIXct(DateTime),
+    Year = year(DateTime),
+    Month = month(DateTime, label = T),
+    DayOfMonth = day(DateTime),
+    DayOfWeek = wday(DateTime),
+    Hour = hour(DateTime)
+  )
+anytime(as.character(crime_samp$Date))
 
-View(head(crime_df_v2,100))
+
+str(crime_samp2)
+lubridate::as_date()
+crime_samp2[, c("originalDate", "Date")]
+
+            
